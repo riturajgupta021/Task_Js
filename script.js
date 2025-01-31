3e31c344cbd8b900e700dac38e08e44a7a74cce6
riturajgupta021 <riturajgupta021@gmail.com>
2025-01-31 11:45:20 +0530

    added

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("Duplicate animal entry is not allowed.");
                 return;
             }
-            if (!animal.size.match(/^(Small|Medium|Large)$/)) {
-                alert("Size must be 'Small', 'Medium', or 'Large'.");
+            if (!animal.size) {
+                alert("Size is required. Please enter a valid size. In ft");
                 return;
             }
             this.animals.push(animal);
@@ -53,18 +53,18 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     const bigCats = [
-        { name: 'Lion', size: 'Large', location: 'Africa', image: 'https://imgs.search.brave.com/f7dlZxMuXI_T0RXIlOlt9hOzgH_6CmQ7LDKYOhsI0jE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvQUIy/Njk2My9waG90by9s/aW9uLmpwZz9zPTYx/Mng2MTImdz0wJms9/MjAmYz1RUzd5bEFX/dktFMHB5a2F6UUFf/WDFQT05qOUpubkxG/eGhnUlN5WFRVUkVR/PQ' },
-        { name: 'Tiger', size: 'Large', location: 'Asia', image: 'https://imgs.search.brave.com/v1dhqz11QH27xugmKSa6-HXxV0tnYEQBi4vJyscuJ8o/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTM2/MjUzMDI0NC9waG90/by90aWdlci1ydW4t/YW5kLWF0dGFjay5q/cGc_cz02MTJ4NjEy/Jnc9MCZrPTIwJmM9/WG1lTWpVeWtyY3Qy/ZHUtejJnZmlPZk85/OVMxUXlqMGpoSDBX/M2t6dTFwcz0' }
+        { name: 'Lion', size: '10 ft', location: 'Africa', image: 'https://imgs.search.brave.com/f7dlZxMuXI_T0RXIlOlt9hOzgH_6CmQ7LDKYOhsI0jE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvQUIy/Njk2My9waG90by9s/aW9uLmpwZz9zPTYx/Mng2MTImdz0wJms9/MjAmYz1RUzd5bEFX/dktFMHB5a2F6UUFf/WDFQT05qOUpubkxG/eGhnUlN5WFRVUkVR/PQ' },
+        { name: 'Tiger', size: '8 ft', location: 'Asia', image: 'https://imgs.search.brave.com/v1dhqz11QH27xugmKSa6-HXxV0tnYEQBi4vJyscuJ8o/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTM2/MjUzMDI0NC9waG90/by90aWdlci1ydW4t/YW5kLWF0dGFjay5q/cGc_cz02MTJ4NjEy/Jnc9MCZrPTIwJmM9/WG1lTWpVeWtyY3Qy/ZHUtejJnZmlPZk85/OVMxUXlqMGpoSDBX/M2t6dTFwcz0' }
     ];
 
     const dogs = [
-        { name: 'Bulldog', size: 'Medium', location: 'UK', image: 'https://imgs.search.brave.com/E7rMFGxFouGE9A7PBQ_u2MGA5-qvWUlItnS_wYRR4Nw/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAzLzgzLzcyLzg0/LzM2MF9GXzM4Mzcy/ODQyMV9TQ243VXF3/Y0x1RFB6YW8zZE1M/QVUzc0xDQ1FxYm1T/dC5qcGc' },
-        { name: 'Beagle', size: 'Small', location: 'USA', image: 'https://imgs.search.brave.com/tptpJJ56EUsw6Dh0tS23MdVi75ipBGsda3vseZDT9sE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNTE5/MDExMzM1L3Bob3Rv/L2JlYWdsZS1wdXBw/eS5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9R0xCRnJZRFY5/a0lFaC12MlBIcHZa/ODRZNmFqczU4VXhr/WFZEVmNrMmczUT0' }
+        { name: 'Bulldog', size: '2 ft', location: 'UK', image: 'https://imgs.search.brave.com/E7rMFGxFouGE9A7PBQ_u2MGA5-qvWUlItnS_wYRR4Nw/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAzLzgzLzcyLzg0/LzM2MF9GXzM4Mzcy/ODQyMV9TQ243VXF3/Y0x1RFB6YW8zZE1M/QVUzc0xDQ1FxYm1T/dC5qcGc' },
+        { name: 'Beagle', size: '2 ft', location: 'USA', image: 'https://imgs.search.brave.com/tptpJJ56EUsw6Dh0tS23MdVi75ipBGsda3vseZDT9sE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNTE5/MDExMzM1L3Bob3Rv/L2JlYWdsZS1wdXBw/eS5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9R0xCRnJZRFY5/a0lFaC12MlBIcHZa/ODRZNmFqczU4VXhr/WFZEVmNrMmczUT0' }
     ];
 
     const bigFish = [
-        { name: 'Shark', size: 'Large', location: 'Ocean', image: 'https://imgs.search.brave.com/Dpq2AXlaIyyohoub50SwpGntwYffdRTnp4AyD6mZZ6I/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA0LzM2LzIxLzAz/LzM2MF9GXzQzNjIx/MDM0OV9MZEhZRjM5/d29tN3J4aDVPODhX/eVdoSTl6d04xcGtM/MS5qcGc' },
-        { name: 'Whale', size: 'Large', location: 'Ocean', image: 'https://imgs.search.brave.com/FgZMgBJJIQ3o5By4LVGGbOJ2NQy232J9bre29xvY5bE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNDk3/MTgxNDMwL3Bob3Rv/L3doYWxlLWluLWhh/bGYtYWlyLmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz1VM29Q/OFJqMVZoUll4V2Y3/bFB0cHJHeUNfNGcy/ZTMxN3lyMFpHczRl/ZEhNPQ' }
+        { name: 'Shark', size: '15 ft', location: 'Ocean', image: 'https://imgs.search.brave.com/Dpq2AXlaIyyohoub50SwpGntwYffdRTnp4AyD6mZZ6I/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA0LzM2LzIxLzAz/LzM2MF9GXzQzNjIx/MDM0OV9MZEhZRjM5/d29tN3J4aDVPODhX/eVdoSTl6d04xcGtM/MS5qcGc' },
+        { name: 'Whale', size: '12 tf', location: 'Ocean', image: 'https://imgs.search.brave.com/FgZMgBJJIQ3o5By4LVGGbOJ2NQy232J9bre29xvY5bE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNDk3/MTgxNDMwL3Bob3Rv/L3doYWxlLWluLWhh/bGYtYWlyLmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz1VM29Q/OFJqMVZoUll4V2Y3/bFB0cHJHeUNfNGcy/ZTMxN3lyMFpHczRl/ZEhNPQ' }
     ];
 
     window.bigCatsTable = new AnimalTable('bigCatsTable', ['name', 'size', 'location']);
